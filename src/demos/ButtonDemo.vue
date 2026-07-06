18cc79e18752cb1adb8863c397cf4ade43a3e847
孙明喆 <sunmingzhe@MacBook-Pro-2.local>
2026-07-06 17:26:52 +0800

    chore: 精简按钮 Playground

--- src/demos/ButtonDemo.vue
+++ src/demos/ButtonDemo.vue
@@ -20,34 +20,16 @@ function increaseThrottledClickCount() {
 
 <template>
   <section class="pg-content">
-    <h2>渐变按钮</h2>
+    <h2>变体样式</h2>
     <div class="demo-block">
       <div class="demo-row">
-        <zq-button variant="gradient">默认</zq-button>
         <zq-button variant="gradient" type="primary">Primary</zq-button>
-        <zq-button variant="gradient" type="success">Success</zq-button>
-        <zq-button variant="gradient" type="warning">Warning</zq-button>
-        <zq-button variant="gradient" type="danger">Danger</zq-button>
-        <zq-button variant="gradient" type="info">Info</zq-button>
-        <zq-button variant="gradient" disabled>Disabled</zq-button>
-      </div>
-    </div>
-
-    <h2 style="margin-top:32px">浅色按钮</h2>
-    <div class="demo-block">
-      <div class="demo-row">
-        <zq-button variant="soft">默认</zq-button>
-        <zq-button variant="soft" type="primary">Primary</zq-button>
         <zq-button variant="soft" type="success">Success</zq-button>
-        <zq-button variant="soft" type="warning">Warning</zq-button>
-        <zq-button variant="soft" type="danger">Danger</zq-button>
-        <zq-button variant="soft" type="info">Info</zq-button>
-        <zq-button variant="soft" disabled>Disabled</zq-button>
+        <zq-button variant="gradient" type="danger" disabled>Disabled</zq-button>
       </div>
     </div>
 
     <h2 style="margin-top:32px">点击控制</h2>
-    <p class="desc">防抖会在最后一次点击后执行，节流会立即执行第一次点击并忽略间隔内的重复点击。</p>
     <div class="demo-block">
       <div class="demo-row">
         <zq-button type="primary" @click="increaseNormalClickCount">
@@ -69,23 +51,6 @@ function increaseThrottledClickCount() {
         >
           节流点击：{{ throttledClickCount }}
         </zq-button>
-      </div>
-      <p class="hint">
-        <code>debounce</code> 与 <code>throttle</code> 只包装 <code>click</code> 事件；同时传入时
-        <code>throttle</code> 优先。
-      </p>
-    </div>
-
-    <h2 style="margin-top:32px">原生按钮</h2>
-    <div class="demo-block">
-      <div class="demo-row">
-        <zq-button>默认</zq-button>
-        <zq-button type="primary">Primary</zq-button>
-        <zq-button type="success">Success</zq-button>
-        <zq-button type="warning">Warning</zq-button>
-        <zq-button type="danger">Danger</zq-button>
-        <zq-button type="info">Info</zq-button>
-        <zq-button disabled>Disabled</zq-button>
       </div>
     </div>
   </section>
